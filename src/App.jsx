e82f920420b08d444a89bbec3d8ce77b8c56b9a7
Ramesh Budha <rawmess977@gmail.com>
2025-08-14 12:55:58 +0530

    custom tailwind theme, utilities and assets added

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,17 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-       <h1 class="text-3xl font-bold underline">    Hello world!  </h1>
-    </>
+    <main>
+       
+        <div className='pattern' />
+         <div>
+          <header className='wrapper'>
+            <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy</h1>
+          </header>
+            <p>Search</p>
+         </div>
+       
+    </main>
   )
 }
 
