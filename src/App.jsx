6a194fc229fdc454bbfe547fb5c36e12c998bec7
Ramesh Budha <rawmess977@gmail.com>
2025-08-28 23:57:03 +0530

    logo and trending section modified

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,7 @@ function App() {
         </header>
 
         {
-           trendingMoviesList.length > 0 && (
+           trendingMoviesList.length > 0 && searchTerm === '' && (
             <section className="trending">
               <h2>Trending Movies</h2>
               <ul>
